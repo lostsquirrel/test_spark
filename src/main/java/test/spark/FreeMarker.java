5a6f3096b1c53cf5efe7b88fad503cf8a8bad3cd
lisong <songli1988@gmail.com>
2016-01-24 22:32:17 +0800

    add freemarker to spark

---
+++ src/main/java/test/spark/FreeMarker.java
@@ -0,0 +1,49 @@
+package test.spark;
+
+import static spark.Spark.get;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.Request;
+import spark.template.freemarker.FreeMarkerEngine;
+
+
+
+public class FreeMarker {
+
+	public static void main(String[] args) {
+		FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
+		Configuration freeMarkerConfiguration = new Configuration();
+		freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(FreeMarker.class, "/"));
+		freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
+		
+
+		
+		// get all post (using HTTP get method)
+		get("/:name", (request, response) -> {
+			Map<String, Object> attributes = new HashMap<>();
+			
+	        attributes.put("name", request.params("name"));
+		    if (shouldReturnHtml(request)) {
+		        response.status(200);
+		        response.type("text/html");
+		        
+		        return freeMarkerEngine.render(new ModelAndView(attributes, "freemarker.ftl"));
+		    } else {
+		        response.status(200);
+		        response.type("application/json");
+		        return attributes;
+		    }
+		});
+	}
+	
+	
+	private static boolean shouldReturnHtml(Request request) {
+	    String accept = request.headers("Accept");
+	    return accept != null && accept.contains("text/html");
+	}
+}
